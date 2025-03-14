8415e8ff16ec04b6cd1a1ac3cdaa6a66b3f049f6
Tofudog <leodefarias25@gmail.com>
2025-03-14 12:36:38 -0400

    Added my documentation page

---
+++ src/pages/Documentation.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+function Documentation() {
+    return <>
+        <h2>
+            Welcome to my documentation page
+        </h2>
+        <Link to="/">back</Link>
+    </>
+}
+
+export default Documentation;
