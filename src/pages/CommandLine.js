ea6d42ff01c6b42925e35855ce551292726e7500
Tofudog <leodefarias25@gmail.com>
2025-03-15 00:20:45 -0400

    Trying to fix user command line

--- src/pages/CommandLine.js
+++ src/pages/CommandLine.js
@@ -2,11 +2,25 @@ import {Link} from 'react-router-dom'
 
 function CommandLine() {
     return <>
-        <h4>
-            Command Documentation:
-            &nbsp;
-            <Link to="/documentation">here</Link>
-        </h4>
+        <div>
+            <h4>
+                Command Documentation:
+                &nbsp;
+                <Link to="/documentation" target="_blank" rel="noopener noreferrer">here</Link>
+            </h4>
+        </div>
+        <div id="flex-container-cmd">
+            <div id="flex-item-cmd">
+                <h4>
+                    hacker123@your-computer %
+                </h4>
+            </div>
+            <div id="flex-item-cmd">
+                <h4>
+                    <input class="mock-command-line"></input>
+                </h4>
+            </div>
+        </div>
     </>
 }
 
