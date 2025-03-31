3c35a4874d1302126dc0e93d4c5f29aa0e664ad5
Tofudog <leodefarias25@gmail.com>
2025-03-30 22:38:16 -0400

    Add some hover filter effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Home from "./pages/Home.js";
 import Documentation from "./pages/Documentation.js";
 
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+// import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/documentation" element={<Documentation />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/documentation" element={<Documentation />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./../styles/Home.css"
 import NavBar from "./NavBar.js";
 import AboutMe from "./AboutMe.js";
@@ -6,10 +8,41 @@ import ContactMe from "./ContactMe.js";
 import Socials from "./Socials.js";
 //import output01 from "../scripts/generate01s.js";
 
+const CircleDiv = ({ size, backgroundColor, borderColor, borderWidth, opacity }) => {
+    const circleStyle = {
+      width: size,
+      height: size,
+      borderRadius: '50%',
+      border: `${borderWidth}px solid ${borderColor}`,
+      backgroundColor: backgroundColor,
+      mixBlendMode: "difference"
+    };
+    return <div style={circleStyle}></div>;
+  };
+
+const BlurredBox = ({ children, x, y }) => {
+    const style = {
+        position: 'absolute',
+        left: x,
+        top: y
+      };
+  
+    return (
+      <div style={style}>
+        {children}
+      </div>
+    );
+  };
+
 function Home() {
     //const section = output01();
+    const [coord, setCoord] = useState({ x: 0, y: 0 });
+    const handleMouseMove = (e) => {
+        setCoord({ x: e.screenX, y: e.screenY });
+    };
+
     return <>
-        <div class="test-flex-container">
+        <div onMouseMove={handleMouseMove} class="test-flex-container">
             <div id="test-space"></div>
             <div>
                 <NavBar></NavBar>
@@ -23,6 +56,10 @@ function Home() {
                 <br></br><br></br>
             </div>
             <div id="test-space"></div>
+            {/* <h1>Mouse coordinates: ({coord.x}, {coord.y})</h1> */}
+            <BlurredBox blurRadius={10} x={coord.x-40} y={coord.y-120}>
+                <CircleDiv size="200px" backgroundColor="rgba(25, 120, 36, 0.288)" borderWidth="200px"></CircleDiv>
+            </BlurredBox>
         </div>
     </>
 }
