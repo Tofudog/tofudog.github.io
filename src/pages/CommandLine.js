1c32f01759bf0672f262413c54661e104418f8cd
Tofudog <leodefarias25@gmail.com>
2025-04-08 20:28:01 -0400

    Fixed styling issues and json dynamic adding

--- src/pages/CommandLine.js
+++ src/pages/CommandLine.js
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import ReactDOM from "react-dom/client";
 import { COMMAND_MAP } from '../constants.js';
 
-import "./../styles/HomeTerminal.module.css";
+import "./../styles/globals.css";
+import "./../styles/HomeTerminal.css";
 
 function CommandLine() {
     const [inputValue, setInputValue] = useState('');
@@ -43,24 +44,26 @@ function CommandLine() {
     };
 
     return <>
-        <br></br><br></br>
-        <div class="vertical-section">
-            <p>Welcome to the Leoterminal</p>
-            <p>Type "<u>help</u>" to run commands and learn more about me</p>
-            <div id="flex-container-cmd">
-                <div>
-                    <p>hacker123@your-computer %</p>
+        <div id="terminal-form">
+            <br></br><br></br>
+            <div class="vertical-section">
+                <p>Welcome to the Leoterminal</p>
+                <p>Type "<u>help</u>" to run commands and learn more about me</p>
+                <div id="flex-container-cmd">
+                    <div>
+                        <p>hacker123@your-computer %</p>
+                    </div>
+                    <div id="flex-item-cmd">
+                        <p>
+                            <form onSubmit={handleSubmit} id="command-form" >
+                                <input placeholder="type a command and hit enter" onChange={handleChange} class="mock-command-line" id="mock-command-line-id"></input>
+                            </form>
+                        </p>
+                    </div>
                 </div>
-                <div id="flex-item-cmd">
-                    <p>
-                        <form onSubmit={handleSubmit} id="command-form" >
-                            <input placeholder="type a command and hit enter" onChange={handleChange} class="mock-command-line" id="mock-command-line-id"></input>
-                        </form>
-                    </p>
+                <div id="command-line-output">
+                    <h4 id="rendered-output"></h4>
                 </div>
-            </div>
-            <div id="command-line-output">
-                <h4 id="rendered-output"></h4>
             </div>
         </div>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import "./../styles/globals.css";
-import "./../styles/Home.module.css";
+import "./../styles/Home.css";
 import NavBar from "./NavBar.js";
 import AboutMe from "./AboutMe.js";
 import CommandLine from "./CommandLine.js";
@@ -16,57 +16,46 @@ import BottomNavbar from "./BottomNavbar.js";
 import mii_logo from "./../assets/images/mii_photo.png";
 import ness_photo from "./../assets/images/ness_photo.png";
 
-/*
-    Previous Home screen (now Leoterminal):
-        /* <div class="test-flex-container">
-            <div id="test-space"></div>
-            <div>
-              <AboutMe></AboutMe>
-              <br></br><br></br><br></br><br></br>
-              <CommandLine></CommandLine>
-            </div>
-            <div id="test-space"></div>
-        </div>
-*/
-
 function Home() {
     return <>
-        <div>
-            <NavBar></NavBar>
-        </div>
-        <div class="space-left">
-            <h1>
-                About Me
-            </h1>
-            <div id="profile-frame">
-                <img id="profile-picture" src={ness_photo} />
-            </div>
-            <h3 id="profile-text">
-                My name is Leonardo de Farias and I am a Computer Science major
-                at Emory University. I like to build projects and learn new things.
-            </h3>
-            <br></br><br></br><br></br><br></br>
+        <div id="home-form">
             <div>
-                <MyExperiences></MyExperiences>
+                <NavBar></NavBar>
             </div>
-            <br></br><br></br><br></br><br></br>
-            <div>
-                <MyProjects></MyProjects>
-            </div>
-            <br></br><br></br><br></br><br></br>
-            <div>
-                <MySkills></MySkills>
-            </div>
-            <br></br><br></br><br></br><br></br>
-        </div>
-        <div id="contact-part">
             <div class="space-left">
-                <MyContactMe></MyContactMe>
+                <h1>
+                    About Me
+                </h1>
+                <div id="profile-frame">
+                    <img id="profile-picture" src={ness_photo} />
+                </div>
+                <h3 id="profile-text">
+                    My name is Leonardo de Farias and I am a Computer Science major
+                    at Emory University. I like to build projects and learn new things.
+                </h3>
+                <br></br><br></br><br></br><br></br>
+                <div>
+                    <MyExperiences></MyExperiences>
+                </div>
+                <br></br><br></br><br></br><br></br>
+                <div>
+                    <MyProjects></MyProjects>
+                </div>
+                <br></br><br></br><br></br><br></br>
+                <div>
+                    <MySkills></MySkills>
+                </div>
+                <br></br><br></br><br></br><br></br>
             </div>
-            <br></br><br></br><br></br>
-        </div>
-        <div id="bottom-part">
-            <BottomNavbar></BottomNavbar>
+            <div id="contact-part">
+                <div class="space-left">
+                    <MyContactMe></MyContactMe>
+                </div>
+                <br></br><br></br><br></br>
+            </div>
+            <div id="bottom-part">
+                <BottomNavbar></BottomNavbar>
+            </div>
         </div>
     </>
 }
